6a787088df29d6a041808421e2acf08c2af917ea
Bhuvaneshkumar <bhuvaneshkumar.r2020ece@sece.ac.in>
2023-07-25 12:20:32 +0530

    array

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hii');
+let c = 5;
+console.log(typeof(c));
+
+
+const arr = [3,4,234 ,423];
+console.log(arr);
+let arr1 = [323 , 43,5446,'weare'];
+let a = arr.length; 
+let array = arr.concat(arr1);
+console.log(array); 
+console.log(a);
